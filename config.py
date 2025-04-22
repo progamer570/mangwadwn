87dec190d58867b5b7765cf4f81aef6b7af09065
Pro gamer era <142508824+progamer570@users.noreply.github.com>
2025-04-22 08:28:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   #Get For @BotFather
   "BOT_TOKEN": "7523890361:AAG8w9eunRfjHlizNmgy7x2dHgodXPfUPt8", 
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "postgresql://postgres:z8VSFBCFcEQ5PXP9@remotely-profound-threadfin.data-1.use1.tembo.io:5432/postgres",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:3Ix0Ny0sNlw1TmZ3@desirably-encouraged-glowworm.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "-1002373950767",
   # Force Subs Channel username without @
